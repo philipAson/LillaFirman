987b1160e1c2bfb5c96553acdf8738a2337b78be
Philip Andersson <philip.andersson.1991@hotmail.com>
2023-06-19 08:59:55 +0200

    Initial Commit

---
+++ LillaFirman/LillaFirmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  LillaFirmanApp.swift
+//  LillaFirman
+//
+//  Created by Philip Andersson on 2023-06-19.
+//
+
+import SwiftUI
+
+@main
+struct LillaFirmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
